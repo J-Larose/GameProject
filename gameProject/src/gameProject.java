0df051b11d39a16e69f5751988af0ff8d5c29701
Jacob Larose <stonejake1205@gmail.com>
2015-04-24 11:25:15 -0400

    added word split method and begining of random choice

--- gameProject/src/gameProject.java
+++ gameProject/src/gameProject.java
@@ -9,9 +9,32 @@
  * @author stone_000
  */
 public class gameProject {
-    
+    /**
+     * 
+     * @param args 
+     */
     public static void main(String[] args) {
-        String wordsArray[] = {""};
-                
+        String wordsArray[] = {"car", "cat", "blue"};
+        int choice = -1;
+        choice = wordChoose(choice);
+        String word=wordsArray[choice];
+        System.out.println(word);
+        char []lettersUnmixed= wordSplit(word);
+        
+    }
+    
+    public static int wordChoose(int choice){
+        choice = (int) ((Math.random() * 3));
+        return choice;
+    }
+    
+    /**
+     * 
+     * @param word
+     * @return 
+     */
+    public static char[] wordSplit(String word ){
+        char[] lettersUnmixed= word.toCharArray();
+        return lettersUnmixed;
     }
 }
